d302d8581882f5e5ed8da97a82cdfacce00bea41
Shantanu Singh Verma <90418655+Shantanu2003@users.noreply.github.com>
2024-06-01 14:52:25 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,11 +1,20 @@
-// add class navbarDark on navbar scroll
 document.addEventListener('DOMContentLoaded', function () {
-    const header = document.querySelector('.navbar');
-    window.addEventListener('scroll', function () {
-        if (window.scrollY >= 100) {
-            header.classList.add('navbarDark');
-        } else {
-            header.classList.remove('navbarDark');
-        }
+        const header = document.querySelector('.navbar');
+        const navbarToggler = document.querySelector('.navbar-toggler');
+
+        window.addEventListener('scroll', function () {
+            if (window.scrollY >= 100) {
+                header.classList.add('navbarDark');
+            } else {
+                header.classList.remove('navbarDark');
+            }
+        });
+
+        navbarToggler.addEventListener('click', function () {
+            if (!header.classList.contains('navbarDark')) {
+                header.classList.add('navbarDark');
+            } else if (window.scrollY < 100) {
+                header.classList.remove('navbarDark');
+            }
+        });
     });
-});
